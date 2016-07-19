37f02b3335e0d9dcec074237567c094c45f5d7ed
BASBK <basbkk1@gmail.com>
2016-07-19 16:59:55 +0300

    Some new stuff! Playing with types.

--- Lesson1/app/src/main/java/com/example/bas_bk/lesson1/Main.java
+++ Lesson1/app/src/main/java/com/example/bas_bk/lesson1/Main.java
@@ -5,6 +5,32 @@ package com.example.bas_bk.lesson1;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!!!");
+        int myNumber = 5;
+
+        double d = 4.5;
+        d = 3.0;
+
+        float f = 4.5f;
+
+        char c = 'g';
+
+        String s1 = new String("Hello, World! ");
+        String s2 = "Hello, World!!!";
+        String s3 = s1+s2;
+
+        int num = 5;
+        String s = "I have " + num + " cookies";
+
+        boolean b = false;
+        b = true;
+
+        boolean toBe = false;
+        b = toBe || !toBe;
+        if (b){
+            System.out.println(toBe);
+        }
+        
+
+        System.out.println(s);
     }
 }
