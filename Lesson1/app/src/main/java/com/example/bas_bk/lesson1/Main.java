4819e3a40c97dcf7fb1d10bea93cf10e0f746a04
BASBK <basbkk1@gmail.com>
2016-07-19 17:06:45 +0300

    Some "if-else" shit. For now it's looks like charp. Little bit borrying thow.

--- Lesson1/app/src/main/java/com/example/bas_bk/lesson1/Main.java
+++ Lesson1/app/src/main/java/com/example/bas_bk/lesson1/Main.java
@@ -5,32 +5,14 @@ package com.example.bas_bk.lesson1;
  */
 public class Main {
     public static void main(String[] args) {
-        int myNumber = 5;
+        int a = 5;
 
-        double d = 4.5;
-        d = 3.0;
-
-        float f = 4.5f;
-
-        char c = 'g';
-
-        String s1 = new String("Hello, World! ");
-        String s2 = "Hello, World!!!";
-        String s3 = s1+s2;
-
-        int num = 5;
-        String s = "I have " + num + " cookies";
-
-        boolean b = false;
-        b = true;
-
-        boolean toBe = false;
-        b = toBe || !toBe;
-        if (b){
-            System.out.println(toBe);
+        if (a == 4){
+            System.out.println("True!");
         }
-        
+        else {
+            System.out.println("False!");
+        }
 
-        System.out.println(s);
     }
 }
