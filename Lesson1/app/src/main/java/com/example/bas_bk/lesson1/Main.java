5249cef4d326e23c3d9819a15a5fc4db464b8537
BASBK <basbkk1@gmail.com>
2016-07-19 17:15:37 +0300

    Some arrays now!

--- Lesson1/app/src/main/java/com/example/bas_bk/lesson1/Main.java
+++ Lesson1/app/src/main/java/com/example/bas_bk/lesson1/Main.java
@@ -1,18 +1,18 @@
 package com.example.bas_bk.lesson1;
 
+import java.util.Arrays;
+
 /**
  * Created by BAS_BK on 19.07.2016.
  */
 public class Main {
     public static void main(String[] args) {
-        int a = 5;
 
-        if (a == 4){
-            System.out.println("True!");
+        int[] arr = {1, 2, 4, 4};
+        for (int i = 0; i < arr.length; i++){
+            System.out.println(arr[i]);
         }
-        else {
-            System.out.println("False!");
-        }
+
 
     }
 }
