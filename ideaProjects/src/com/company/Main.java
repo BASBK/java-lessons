558851d711724274355d8c7277953736cef6370b
BASBK <basbkk1@gmail.com>
2016-07-19 18:14:02 +0300

    Working with compilators

--- ideaProjects/src/com/company/Main.java
+++ ideaProjects/src/com/company/Main.java
@@ -3,22 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(sum(5,3));
-        System.out.println(sum(1.2,2.9));
-        Student s = new Student();
-        s.setName("Вова");
-        s.setLastName("Горохов");
-        s.printFullName();
+        Point p = new Point(4, 2);
+        p.printPoint();
 
-
-    }
-    public static void foo(){
-
-    }
-    public static int sum(int a, int b){
-        return a+b;
-    }
-    public static double sum(double a, double b){
-        return a+b;
     }
 }
