284eb553075d2def183a0f26354c6a4201023c2b
BASBK <basbkk1@gmail.com>
2016-07-19 16:46:14 +0300

    The first java programm. "Hello world" ofcource!

---
+++ Lesson1/app/src/main/java/com/example/bas_bk/lesson1/Main.java
@@ -0,0 +1,10 @@
+package com.example.bas_bk.lesson1;
+
+/**
+ * Created by BAS_BK on 19.07.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!!!");
+    }
+}
