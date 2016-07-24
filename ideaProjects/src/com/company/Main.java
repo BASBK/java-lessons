cd3840dd7834728555e95a8d8d217fd6a6cccfbc
BASBK <basbkk1@gmail.com>
2016-07-24 13:20:18 +0300

    Немношка наследования.

--- ideaProjects/src/com/company/Main.java
+++ ideaProjects/src/com/company/Main.java
@@ -3,8 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Point p = new Point(4, 2);
-        p.printPoint();
+        Shape s1 = new Circle(5.0);
+        Shape s2 = new Rectangle(5.0, 4.0);
+        Shape larger = getLargerShape(s1, s2);
 
+        System.out.println("The area of the largest shape is: " + larger.area());
+
+    }
+
+    public static Shape getLargerShape(Shape s1, Shape s2){
+        if (s1.area() > s2.area())
+            return s1;
+        else
+            return s2;
     }
 }
